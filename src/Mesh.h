b0de37035b8f090fcba540b38743e07d0d990061
Tommaso Galatolo <tommaso.galatolo@gmail.com>
2015-02-12 23:54:10 +1100

    Changelog:
    Added assimp (Open Asset Import Library) to project to load complex model.
    
    Added Mesh class and Model class, to test new lib. Note: Push from school did not went all the way through, it might ended with an error. Merged is necessary since we do not use SOIL as a standard to load our texture. Uniform code to other lib.

---
+++ src/Mesh.h
@@ -0,0 +1,55 @@
+#ifndef _MESH_H_
+#define _MESH_H_
+//Libs
+//Window and GL
+#include "gl_core_4_4.h"
+//Math
+#define GLM_SWIZZLE
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+//Other
+#include "ShaderCompiler.h"
+#include "Importer.hpp"
+#include "scene.h"
+#include "postprocess.h"
+//std
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using glm::vec2;
+using glm::vec4;
+using std::string;
+using std::vector;
+
+struct mVertex
+{
+	vec4 m_vPos;
+	vec4 m_vNormal;
+	vec2 m_vTextCoord;
+};
+
+struct Texture {
+	GLuint m_uiId;
+	string m_sType; 
+	aiString path;
+};
+
+class Mesh
+{
+public:
+	/*  Mesh Data  */
+	vector<mVertex> m_voVertices;
+	vector<GLuint>	m_voIndices;
+	vector<Texture> m_voTextures;
+	/*  Functions  */
+	Mesh(vector<mVertex> vertices, vector<GLuint> indices, vector<Texture> textures);
+	void Draw(ShaderCompiler m_glslShader);
+private:
+	/*  Render data  */
+	GLuint VAO, VBO, IBO;
+	/*  Functions    */
+	void setupMesh();
+};
+#endif //!_MESH_H_
