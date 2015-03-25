52ca7449dbeefc2fa3840d918200f0313c69ec47
Tommaso Galatolo <tommaso.galatolo@gmail.com>
2015-03-26 07:46:09 +1100

    Changelog:
    Added leap motion .dll and dependency to project in order to do some hand motion tracking example.
    Added .h for procedurally generated fractal terrain. I'm still researching algorithms and how to implement a fast and reliable noise beside Perlin.

---
+++ src/PGFT.h
@@ -0,0 +1,17 @@
+#ifndef _PGFT_H_
+#define _PGFT_H_
+//Lib
+#include "Application.h"
+
+class PGFT
+{
+private:
+	vec4 m_vMousePos;
+	float m_fSampleRate;
+
+public:
+	PGFT();
+	~PGFT();
+
+};
+#endif //!_PGFT_H_
